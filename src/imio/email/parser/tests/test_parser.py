a02c333385ca58cfb00efdf4f0b5c1f947f3caaa
Stephan Geulette <s.geulette@imio.be>
2021-03-26 14:47:29 +0100

    Added failing test

--- src/imio/email/parser/tests/test_parser.py
+++ src/imio/email/parser/tests/test_parser.py
@@ -16,8 +16,18 @@ class TestParser(unittest.TestCase):
 
     def test_extract_relevant_message(self):
         to_tests = [{'fn': '01_email_containing_eml.eml', 'orig': 'Agent forward',
+                     'opl': ['multipart/alternative', 'message/rfc822'],
+                     'opl1': ['multipart/mixed'],
+                     'ipl': ['multipart/related', 'image/png', 'application/vnd.oasis.opendocument.text'],
                      'msg_s': 'Bonjour,\n\nVoici une phrase avec un petit logo [image: image.png] collé dans le texte.',
-                     }]
+                     },
+                    {'fn': '02_email_containing_eml_containing_eml.eml', 'orig': 'Agent forward',
+                     'opl': ['multipart/alternative', 'message/rfc822'],
+                     'opl1': ['multipart/mixed'],
+                     'ipl': ['text/plain', 'message/rfc822'],
+                     'msg_s': 'Dear user,',
+                     },
+                    ]
         for dic in to_tests:
             name = dic['fn']
             # with self.subTest(name=name): errors not returned in zc.recipe.testrunner
@@ -25,5 +35,7 @@ class TestParser(unittest.TestCase):
             omsg = oparsed.message
             iparsed = Parser(omsg)
             self.assertEqual(iparsed.origin, dic['orig'], name)
-            self.assertTrue(iparsed.parsed_message.body.startswith(dic['msg_s']))
-            # payload iparsed: ['multipart/related', 'image/png', 'application/vnd.oasis.opendocument.text']
+            self.assertListEqual([part.get_content_type() for part in omsg.get_payload()], dic['opl'], name)
+            part1 = omsg.get_payload()[1]
+            self.assertListEqual([part.get_content_type() for part in part1.get_payload()], dic['opl1'], name)
+            self.assertTrue(iparsed.parsed_message.body.startswith(dic['msg_s']), name)
